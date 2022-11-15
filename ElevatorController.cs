ebd9369ce7a85253a9bd7476d04f092d7739b32d
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2022-11-15 16:57:39 -0500

    list of elevator status api connected to database

---
+++ ElevatorController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RocketApi.Models;
+
+namespace ElevatorApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class ElevatorController : ControllerBase
+{  
+    private readonly ElevatorContext _context;
+
+    public ElevatorController(ElevatorContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Elevator>>> GetElevatorItems()
+    {
+        return await _context.elevators.ToListAsync();
+    }
+
+    private IActionResult View()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET: api/Elevator/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+    // PUT: api/Elevator/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
